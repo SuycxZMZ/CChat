55f13b353107deca56f14eb418cfe16502f68968
SuycxZMZ <suycx65@163.com>
2024-10-20 20:38:03 +0800

    加入ioServicePool的类和逻辑，多线程处理网络收发

--- GateServer/CServer.cpp
+++ GateServer/CServer.cpp
@@ -1,5 +1,6 @@
 #include "CServer.h"
 #include "HttpConnection.h"
+#include "AsioIOServicePool.h"
 #include <iostream>
 #include <exception>
 
@@ -13,7 +14,9 @@ CServer::CServer(boost::asio::io_context& ioc, unsigned short& port) :
 
 void CServer::Start() {
 	auto self = shared_from_this();
-	_acceptor.async_accept(_socket, [self](beast::error_code ec) {
+	auto& io_context = AsioIOServicePool::GetInstance()->GetIOService();
+	auto new_connection = std::make_shared<HttpConnection>(io_context);
+	_acceptor.async_accept(new_connection->GetSocket(), [self, new_connection](beast::error_code ec) {
 		try {
 			if (ec) {
 				// 一个连接失败，继续监听其他连接
@@ -21,14 +24,15 @@ void CServer::Start() {
 				return;
 			}
 
-			// 创建新连接，并创建一个HttpConnection类来管理连接
-			std::make_shared<HttpConnection>(std::move(self->_socket))->Start();
+			// 连接建立之后就要开始接收了
+			new_connection->Start();
 
 			// 继续监听
 			self->Start();
 		}
 		catch (std::exception& exp) {
 			std::cout << "exception is:" << exp.what() << std::endl;
+			self->Start();
 		}
 	});
 }
