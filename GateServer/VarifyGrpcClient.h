9fc5beb30ee102de16fb4e6742235ca86ac5a89b
SuycxZMZ <suycx65@163.com>
2024-10-22 10:51:28 +0800

    grp连接池逻辑修改

--- GateServer/VarifyGrpcClient.h
+++ GateServer/VarifyGrpcClient.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <grpcpp/grpcpp.h>
+#include <atomic>
 #include "message.grpc.pb.h"
 #include "const.h"
 #include "Singleton.h"
@@ -13,6 +14,26 @@ using message::GetVarifyReq;
 using message::GetVarifyRsp;
 using message::VarifyService;
 
+class RPCConnectionPool {
+public:
+	RPCConnectionPool(std::size_t poolsize, std::string host, std::string port);
+	~RPCConnectionPool();
+	std::unique_ptr<VarifyService::Stub> GetConnection();
+
+	// 回收一个连接到pool里
+	void ReturnConnection(std::unique_ptr<VarifyService::Stub> context);
+	void Close();
+
+private:
+	std::atomic<bool> _b_stop;
+	std::size_t _pool_size;
+	std::string _host;
+	std::string _port;
+	std::mutex _mtx;
+	std::condition_variable _cond;
+	std::queue<std::unique_ptr<VarifyService::Stub>> _connections;
+};
+
 class VarifyGrpcClient : public Singleton<VarifyGrpcClient>
 {
 	friend class Singleton<VarifyGrpcClient>;
@@ -22,6 +43,6 @@ private:
 	VarifyGrpcClient();
 
 private:
-	std::unique_ptr<VarifyService::Stub> _stub;
+	std::unique_ptr<RPCConnectionPool> _rpc_connection_pool;
 };
 
