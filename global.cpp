f638cfe883b83c2bbff4aebb4b0432b4328ad05b
SuycxZMZ <suycx65@163.com>
2024-10-23 20:01:59 +0800

    添加密码加密和错误信息提示

--- global.cpp
+++ global.cpp
@@ -8,3 +8,13 @@ std::function<void(QWidget*)> repolish = [](QWidget* w) {
 };
 
 QString gate_url_prefix = "";
+
+std::function<QString(QString)> xorString = [](QString input) -> QString {
+    QString ans = input;
+    int len = input.length();
+    ushort xor_code = len % 255;
+    for (int i = 0; i < len; ++i) {
+        ans[i] = QChar(static_cast<ushort>(input[i].unicode() ^ xor_code));
+    }
+    return ans;
+};
